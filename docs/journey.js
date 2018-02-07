477962d5a792e4acea9d8d186ae9018a33e44b58
Matias Grioni <matgrioni@gmail.com>
2018-02-06 20:13:34 -0500

    Some starting code for frontend.

--- docs/journey.js
+++ docs/journey.js
@@ -1,1 +1,22 @@
-// Unused for now.
+$(document).ready(function() {
+  var queryResultsTemplate = Handlebars.compile($('#queries-template').html());
+
+  var query = $('#query');
+  var queryResults = $('#query-results');
+
+  var testQueries = [
+    {
+      'city': 'Buenos Aires',
+      'country': 'Argentina',
+      'content': 'A beautiful European flavored city in the heart of South America'
+    }
+  ];
+
+  const context = {
+    queries: testQueries
+  };
+
+  query.on('click', function() {
+    queryResults.html(queryResultsTemplate(context));
+  });
+});
